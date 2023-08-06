57ba2165dbdde0b42a01fee258ed59de57b6141f
dhiajlali <135439641+dhiajlali@users.noreply.github.com>
2023-08-06 20:27:56 +0100

    Initial commit

---
+++ index.ts
@@ -0,0 +1,8 @@
+//######################################################################
+//###########################           SSTech       ###################
+//###########################   Programming Courses  ###################
+//######################################################################
+let maynumber : number;
+maynumber = parseInt(prompt("enter un numero enter 1 ET 3));
+while(maynumber>=1 && maynumber<=3){maynumber=parseInt(prompt("enter un numero enter 1 ET 3" ))};
+console.log(maynumber);
